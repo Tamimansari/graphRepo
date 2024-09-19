b26bac7625ed9729956b1c1db10c39489b061787
Tamimansari <tamimansari7339@gmail.com>
2024-09-19 14:55:20 +0530

    commit on main for merge

--- graph.js
+++ graph.js
@@ -1,1 +1,45 @@
 new entrys
+return {
+    currentRepo : Lyte.attr("string",{default: ""}),
+    commitStatus : Lyte.attr("object",{default: ""}),
+    hasPath : Lyte.attr('boolean' , {default : true}),
+    checkAllTrackedFiles : Lyte.attr('boolean' , {
+        default : false,
+    }),
+    gitStatus : Lyte.attr('object' , {
+        default :  {
+            trackedFiles: [
+                'component/comp1/66.txt',
+                'component/comp2/mm.txt',
+                'component/comp3/n5.txt',
+                'component/comp4/n55.txt',
+                'package.json'
+            ],
+            untrackedFiles: [
+                'component/comp7/utils.js',
+                'component/comp7df/utils.js',
+                'component/comp7asfd/utils.js',
+                'component/comp74t/utils.js',
+                'component/comp7etrv/utils.js',
+                'component/compew7/utils.js',
+                'component/comp1/vfx.js'
+            ]
+       }
+    }),
+    filesList : Lyte.attr('array',{
+        default : []
+    }),
+
+    graphList : Lyte.attr('array' , {
+        default : []
+           
+    }),
+     
+    selectedBranches : Lyte.attr('array' , {
+        default : []
+    })
+
+}
+function name(){
+    console.log("hi")
+}
